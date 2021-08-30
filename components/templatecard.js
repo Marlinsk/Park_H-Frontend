47bb9abd1c2749f3d69c9a8052a605d67a7b4891
Marlon <Marlonnlmx@outlook.com>
2021-08-30 18:24:39 -0300

    Update to feed and component

--- components/templatecard.js
+++ components/templatecard.js
@@ -1,122 +1,54 @@
 const templateCard = (item) => {
   return `
-  <div class="col-md-7 mx-auto">
+  <div class="col-lg-6 mx-auto">
 
-    <div class="card mb-4">
-      <h4 class="card-header text-center">${item?.tituloAnuncio}</h4>
-      <div class="card-body text-dark">
-        <div class="tab-content" id="pills-tabContent">
-          <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-            aria-labelledby="pills-home-tab">
-            <h5 class="card-title">Tipo de Garagem</h5>
-            <p class="card-text mb-3">${item?.tipoLogradouroString}</p>
-            <h5 class="card-title">Descrição</h5>
-            <p class="card-text">${item?.descricaoLocal}</p>
-          </div>
-          <div class="tab-pane fade" id="pills-profile" role="tabpanel"
-            aria-labelledby="pills-profile-tab">
-            <div class="mb-3">
-              <div class="row justify-content-start">
-                <div class="col-4">
-                  <h5 class="card-title">Cidade</h5>
-                  <p class="card-text">${item?.cidade}</p>
-                </div>
-                <div class="col-3">
-                  <h5 class="card-title">Estado</h5>
-                  <p class="card-text">${item?.estado}</p>
-                </div>
-                <div class="col-3">
-                  <h5 class="card-title">Bairro</h5>
-                  <p class="card-text">${item?.bairro}</p>
-                </div>
-              </div>
+    <div class="card">
+      <div class="card-body">
+        <div class="container">
+
+          <a href="#" class="text-decoration-none">
+            <h5 class="card-title">${item?.tituloAnuncio}</h5>
+          </a>
+
+          <div class="row justify-content-start mb-2">
+            <div class="col-5">
+              <p class="card-text">${item?.rua}</p>
             </div>
-            <div class="mb-3">
-              <div class="row justify-content-start">
-                <div class="col-4">
-                  <h5 class="card-title">Rua</h5>
-                  <p class="card-text">${item?.rua}</p>
-                </div>
-                <div class="col-3">
-                  <h5 class="card-title">Número</h5>
-                  <p class="card-text">${item?.numero}</p>
-                </div>
-                <div class="col-3">
-                  <h5 class="card-title">CEP</h5>
-                  <p class="card-text">${item?.cep}</p>
-                </div>
-              </div>
+            <div class="col-2">
+              <p class="card-text">Nº: ${item?.numero}</p>
             </div>
-            <div class="mb-3">
-              <div class="row justify-content-start">
-                <div class="col-4">
-                  <h5 class="card-title">Complemento</h5>
-                  <p class="card-text">${item?.complementoLocal}</p>
-                </div>
-              </div>
+            <div class="col-4">
+              <p class="card-text">Bairro: ${item?.bairro}</p>
+            </div>
+            <div class="col-4">
+              <p class="card-text">Cidade: ${item?.cidade}</p>
+            </div>
+            <div class="col-4">
+              <p class="card-text">Estado: ${item?.estado}</p>
             </div>
           </div>
-          <div class="tab-pane fade" id="pills-contact" role="tabpanel"
-            aria-labelledby="pills-contact-tab">
-            <div class="row">
-              <div class="col">
-                <h5 class="card-title">Tipo de Cobrança</h5>
-                <p class="card-text">${item?.tipoCobranca}</p>
-              </div>
-              <div class="col">
-                <h5 class="card-title">Preço do Aluguel</h5>
-                <p class="card-text">${item?.preco}</p>
-              </div>
+
+          <div class="row justify-content-start mb-2">
+            <div class="col-5">
+              <p class="card-text">${item?.tipoLogradouroString}</p>
             </div>
           </div>
+
+          <div class="row justify-content-start mb-2">
+            <div class="col-5">
+              <p class="card-text">${item?.tipoCobranca}</p>
+            </div>
+            <div class="col-5">
+              <p class="card-text">${item?.preco}</p>
+            </div>
+          </div>
+
         </div>
       </div>
-      <div class="card-footer">
-        <ul class="nav nav-pills justify-content-center" id="pills-tab" role="tablist">
-          <li class="nav-item" role="presentation">
-            <button class="nav-link btn-outline-primary" id="pills-home-tab" data-bs-toggle="pill"
-              data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
-              aria-selected="false">Sobre</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link btn-outline-primary" id="pills-profile-tab"
-              data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab"
-              aria-controls="pills-profile" aria-selected="false">Endereço</button>
-          </li>
-          <li class="nav-item" role="presentation">
-            <button class="nav-link btn-outline-primary" id="pills-contact-tab"
-              data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab"
-              aria-controls="pills-contact" aria-selected="false">Preço</button>
-          </li>
-          <li class="dropdown">
-            <button class="btn" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
-              aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-three-dots" viewBox="0 0 16 16">
-                <path
-                  d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
-              </svg>
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <button type="button" class="dropdown-item" data-bs-toggle="modal"
-                  data-bs-target="#staticBackdrop1">
-                  Editar Anúncio
-                </button>
-              </li>
-              <li>
-                <button type="button" class="dropdown-item" data-bs-toggle="modal"
-                  data-bs-target="#staticBackdrop2">
-                  Excluir Anúncio
-                </button>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
     </div>
+
   </div>
-` 
+`
 }
 
 export { templateCard };
