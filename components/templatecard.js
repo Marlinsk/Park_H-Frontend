0674c1005fe03d855a247dc5cf4a291c13c09794
Marlon <Marlonnlmx@outlook.com>
2021-09-27 08:43:16 -0300

    New design card

--- components/templatecard.js
+++ components/templatecard.js
@@ -1,7 +1,7 @@
 const templateCard = (item) => {
   return `
   <div class="col-lg-6 mx-auto">
-
+    
     <div class="card mb-4">
       <div class="card-body">
         <div class="container">
@@ -12,30 +12,6 @@ const templateCard = (item) => {
 
           <div class="row justify-content-start mb-2">
             <div class="col-4">
-              <p class="card-text">${item?.rua}</p>
-            </div>
-            <div class="col-2">
-              <p class="card-text">Nº: ${item?.numero}</p>
-            </div>
-            <div class="col-4">
-              <p class="card-text">Bairro: ${item?.bairro}</p>
-            </div>
-            <div class="col-4">
-              <p class="card-text">Cidade: ${item?.cidade}</p>
-            </div>
-            <div class="col-4">
-              <p class="card-text">Estado: ${item?.estado}</p>
-            </div>
-          </div>
-
-          <div class="row justify-content-start mb-2">
-            <div class="col-5">
-              <p class="card-text">${item?.tipoLogradouroString}</p>
-            </div>
-          </div>
-
-          <div class="row justify-content-start mb-2">
-            <div class="col-3">
               <p class="card-text">${item?.tipoPagamento}</p>
             </div>
             <div class="col-5">
@@ -46,7 +22,7 @@ const templateCard = (item) => {
         </div>
       </div>
     </div>
-
+  
   </div>
 `
 }
