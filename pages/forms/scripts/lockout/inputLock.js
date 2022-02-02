d09ecaacc8c009e9aeacd87658411428db02d8f6
Marlon <Marlonnlmx@outlook.com>
2022-02-02 13:31:08 -0300

    Launching code

---
+++ pages/forms/scripts/lockout/inputLock.js
@@ -0,0 +1,35 @@
+$("#nome-completo", "#titulo-anuncio", "#estado").on('keypress', function (event) {
+  var regex = new RegExp("^[a-zA-Z-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇ ]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
+
+$("#nome-do-banco", "#descricao", "#nome-da-cidade", "#bairro", "#nome-da-rua", "#complemento").on('keypress', function (event) {
+  var regex = new RegExp("^[a-zA-Z-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇ0-9 ]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
+
+$("#numero-residencia").on('keypress', function (event) {
+  var regex = new RegExp("^[a-zA-Z0-9 ]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
+
+$("#capacidade").on('keypress', function (event) {
+  var regex = new RegExp("^[0-9 ]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
