33e92867ecbf312bb047afe6541136fe5d7a0858
Marlon <Marlonnlmx@outlook.com>
2022-07-13 23:40:41 -0300

    Style card update

--- components/templatecard.js
+++ components/templatecard.js
@@ -1,16 +1,18 @@
 const templateCard = (item) => {
   return `
-  <div class="col-lg-6 mx-auto">
-    <div class="card text-white bg-dark mb-4" style="max-width: 56rem;">
-        <div class="card-header bg-transparent border-secondary text-center">${item?.tituloAnuncio}</div>
-        <div class="card-body">
-            <h5 class="card-title">${item?.cidade} - ${item?.estado}</h5>
-            <p class="card-text">Rua: ${item?.rua} - Bairro: ${item?.bairro} - Nº: ${item?.numero} - CEP: ${item?.cep} </p>
-        </div>
-        <div class="card-footer bg-transparent border-secondary fw-bolder">Tipo da Cobrança: ${item?.tipoPagamento} / Preço: ${item?.preco}</div>
+  <div class="col-lg-7 mx-auto">
+    <div class="card">
+      <div class="card-body">
+        <a href="#" class="h5 mb-1" style="display: flex; align-items: center; text-decoration: none;">
+          <img src="../../node_modules/bootstrap-icons/icons/house-fill.svg" alt="house-fill" width="26px"
+            height="26px" style="padding-right: 7px;">
+          ${item?.tituloAnuncio}
+        </a>
+        <p class="card-subtitle">${item?.rua}, ${item?.numero} - ${item?.bairro}, ${item?.cidade} - ${item?.estado}, ${item?.cep}</p>
+      </div>
     </div>
   </div>
-`
-}
+`;
+};
 
 export { templateCard };
